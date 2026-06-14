964d09ac537d5e663cce1207ec06f04551326707
S <sidhividyaparida@gmail.com>
2026-06-14 19:38:34 +0530

    Using CRUD Operations

--- main.py
+++ main.py
@@ -3,13 +3,42 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
+todos = []
 
-@app.post("/new user")
-def new_user(user: User):
-    return{
-        "message": "User created successfully",
-        "data": user
-         }
+class Todo(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
+# Create API 
+@app.post("/todos")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo created successfully", "todo": todo}
+# Read API
+@app.get("/todos")
+def get_todos():
+    return todos
+# get/read single data
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+    return {"error": "Todo not found"}
+# Update API
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: Todo):
+    for idx, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[idx] = updated_todo
+            return {"message": "Todo updated successfully", "data": updated_todo}
+    return {"error": "Todo not found"}
+# Delete API
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for idx, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos.pop(idx)
+            return {"message": "Todo deleted successfully"}
+    return {"error": "Todo not found"}
