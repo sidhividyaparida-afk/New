46cbd8fe29e9c794277eb7f0bb1c80a666359b86
S <sidhividyaparida@gmail.com>
2026-06-22 14:45:31 +0530

    Middleware Flow

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
+from fastapi import FastAPI, Request
+import time
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
 
-@app.post("/new user")
-def new_user(user: User):
-    return{
-        "message": "User created successfully",
-        "data": user
-         }
+    print(f"Path: {request.url.path} | Method: {request.method} | Status Code: {response.status_code} | Process Time: {process_time} seconds")
+
+    return response
+
+@app.middleware("http")
+async def add_custom_header(request: Request, call_next):
+    print("Request recieved")
+    response = await call_next(request)
+    response.headers["X-Custom-Header"] = "Custom Value"
+    print("Response sent with custom header")
+    return response
