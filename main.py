565053dee8fc2e56ac05a245e92cc052f5e1c358
S <sidhividyaparida@gmail.com>
2026-06-26 12:36:36 +0530

    SQLAlchemy

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
+DATABASE_URL = "sqlite:///./test.db"
 
-@app.post("/new user")
-def new_user(user: User):
-    return{
-        "message": "User created successfully",
-        "data": user
-         }
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False})
+
+sessionLocal = sessionmaker(bind=engine)
+
+base = declarative_base()
+
+class Todo(base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Integer, default=0)
+
+base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home(db: Session = Depends(get_db)):
+    return {"message": "DB connected successfully!"}
