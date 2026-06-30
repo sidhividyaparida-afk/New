95c601f486bea147b64adbcafd597f9c5b9d04e9
S <sidhividyaparida@gmail.com>
2026-06-30 16:37:19 +0530

    Async Programming

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import time, asyncio
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
-
-@app.post("/new user")
-def new_user(user: User):
+@app.get("/")
+async def home():
+    await asyncio.sleep(3)
     return{
-        "message": "User created successfully",
-        "data": user
-         }
+        "message":"Async API"
+    }
+# def fun():
+#     time.sleep(3)
+#     return "Function executed after 3 seconds"
+# 
+# print(fun())
+
+# async def fun():
+#     await asyncio.sleep(3)
+#     return "Function executed after 3 seconds"
+
+# print(asyncio.run(fun()))
